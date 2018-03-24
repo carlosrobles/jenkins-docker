4c6e8bb1d6f5742689f652251946642b86c79602
Carlos <gbmcarlos@gmail.com>
2018-03-24 12:35:23 +0400

    mount a volueme for the ssh keys

--- up.sh
+++ up.sh
@@ -9,7 +9,9 @@ echo ">>>> Removing old container"
 docker rm -f jenkins || true
 
 echo ">>>> Running new container"
-docker run --name jenkins -d -p 8080:8080 -p 50000:50000 -v ~/jenkins_home:/var/jenkins_home sam/jenkins:latest
+docker run --name jenkins -d -p 8080:8080 -p 50000:50000 \
+    -v ~/jenkins_home:/var/jenkins_home sam/jenkins:latest \
+    -v ~/.ssh:/var/jenkins_home/.ssh
 
 echo ">>>> Tailing logs"
 docker logs -f jenkins
