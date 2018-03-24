22f7b005135839caaa3263cd24ef8b2a500a607f
Carlos <gbmcarlos@gmail.com>
2018-03-24 12:46:02 +0400

    remove vendor name

--- up.sh
+++ up.sh
@@ -3,7 +3,7 @@
 echo ">>>> Moving to $(dirname "$0")"
 cd "$(dirname "$0")"
 echo ">>>> Building docker image"
-docker build -t sam/jenkins:latest .
+docker build -t jenkins:latest .
 
 echo ">>>> Removing old container"
 docker rm -f jenkins || true
@@ -11,7 +11,7 @@ docker rm -f jenkins || true
 echo ">>>> Running new container"
 docker run --name jenkins -d -p 8080:8080 -p 50000:50000 \
     -v ~/jenkins_home:/var/jenkins_home \
-    -v ~/.ssh:/var/jenkins_home/.ssh sam/jenkins:latest
+    -v ~/.ssh:/var/jenkins_home/.ssh jenkins:latest
 
 echo ">>>> Tailing logs"
 docker logs -f jenkins
